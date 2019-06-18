c27b895a0acc8fcbb974af30767f40c29c7b202d
Julieth Katherine Riveros Galeano <julieth.riveros.g@gmail.com>
2019-06-17 22:42:14 -0500

    Solved question 1 with python

---
+++ basic/Question1.py
@@ -0,0 +1,13 @@
+#Question1: Accept two int values from user and return their product.
+#if the product is greater than 1000, then return their sum.
+
+#Puedo usar la función input para esperar que el usuario ingrese un String.
+a = int(input("Ingrese el número a: "))
+b = int(input("Ingrese el número b: "))
+suma = 0
+producto = a*b
+if (producto > 1000):
+	suma = a+b
+	print("la suma es", suma)
+else:	
+	print("el producto es", producto)
