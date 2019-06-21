79da4ba69aa569f694cb6ae98073b6a954e84f76
Julieth Katherine Riveros Galeano <julieth.riveros.g@gmail.com>
2019-06-20 20:17:18 -0500

    I created the basic program that says Hello.

---
+++ basic/Question0.py
@@ -0,0 +1,4 @@
+# Create a program that recives a name and says Hello.
+import sys
+
+print("hello",sys.argv[1])
---
+++ basic/Question0.r
@@ -0,0 +1,6 @@
+## Create a program that recives a name and says Hello.
+
+args <- commandArgs(trailingOnly = TRUE)
+
+cat("Hello",args[2],"\n")
+
